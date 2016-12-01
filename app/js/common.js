a0cc83481a127f914b7ecc1e638c02e920c12325
KudelAndrei <kudel1995@mail.ru>
2016-12-02 02:04:16 +0300

    header

--- app/js/common.js
+++ app/js/common.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
 	//$(".service-item h4").equalHeights(); 
 
 	//Слайдер  https://gist.github.com/KudelAndrei/fca3e6e73e1e0aa21996a12af58c9069
-	$(".sl").slick({
+	$(".sl-header").slick({
 		arrows: true,
-		dots: true,
 	});
 
 	//плавный скрол в хроме
