018221ebf0d5dd8599ad96e9c1ea791b4db06fdc
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2016-12-02 10:54:01 +0300

    finish header

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 	//Слайдер  https://gist.github.com/KudelAndrei/fca3e6e73e1e0aa21996a12af58c9069
 	$(".sl-header").slick({
 		arrows: true,
+		prevArrow: '<div class="sl-arrow sl-left"><i class="fa fa-angle-left" aria-hidden="true"></i></div>', 
+		nextArrow: '<div class="sl-arrow sl-right"><i class="fa fa-angle-right" aria-hidden="true"></i></div>', 
 	});
 
 	//плавный скрол в хроме
